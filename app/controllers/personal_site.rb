c28a09382b3f3963ba7930d168cc4bb7405b7b78
Ali Schlereth <aschlere@sbcglobal.net>
2017-11-03 15:54:51 -0600

    Add ability to show error page to PersonalSite

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -3,7 +3,18 @@ require 'rack'
 class PersonalSite
 
   def self.call(env)
-    
+    case env["PATH_INFO"]
+    when '/' then index
+    else
+      error
+    end
+  end
+
+  def index
     ['200', {'Content-Type' => 'text/html'}, [File.read('./app/views/index.html')]]
+  end
+
+  def error
+    ['400', {'Content-Type' => 'text/html'}, File.read('./app/views/error.html')]
   end
 end
